c9d286a80955d6f71faa9a74b42e76f86a9bbfac
徐文见 <2503963174@qq.com>
2021-10-07 19:40:26 +0800

    feat：http练习

---
+++ httpserver/go.mod
@@ -0,0 +1,5 @@
+module httpserver
+
+go 1.14
+
+require github.com/golang/glog v1.0.0 // indirect
---
+++ httpserver/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	//"strings"
+	"github.com/golang/glog"
+	_ "net/http/pprof"
+)
+
+func main() {
+
+	flag.Set("v", "4")
+	glog.V(2).Info("Starting http server...")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/header", rootHandler)
+	mux.HandleFunc("/healthz", healthz)
+	mux.HandleFunc("/version", GetVeriosn)
+	// mux.HandleFunc("/debug/pprof/", pprof.Index)
+	// mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	// mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	// mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	err := http.ListenAndServe(":80", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func healthz(w http.ResponseWriter, r *http.Request) {
+	glog.V(2).Info("接口信息：healthz"," ip:",r.RemoteAddr," 状态码",http.StatusOK)
+	io.WriteString(w, "200\n")
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	glog.V(2).Info("接口信息：获取请求头"," ip:",r.RemoteAddr," 状态码",http.StatusOK)
+	for k, v := range r.Header {
+		w.Header().Set(k,strings.Join(v,","))
+	}
+	//w.WriteHeader(http.StatusBadRequest)
+}
+
+func GetVeriosn(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("entering GetVeriosn")
+	glog.V(2).Info("接口信息：GetVeriosn"," ip:",r.RemoteAddr," 状态码",http.StatusOK)
+	path:=os.Getenv("GOPATH")
+	w.Header().Set("GOPATH", path)
+}
