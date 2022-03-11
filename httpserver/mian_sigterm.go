334667f1bd5144b2ebdb9eaaaa4bfb273be12943
fireman-coder <73879578+fireman-coder@users.noreply.github.com>
2022-03-11 17:58:09 +0800

    优雅终止

---
+++ httpserver/mian_sigterm.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	//_ "net/http/pprof"
+
+	"github.com/golang/glog"
+)
+
+func main() {
+	flag.Set("v", "4")
+	glog.V(2).Info("Starting http server...")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/header", rootHandler1)
+	mux.HandleFunc("/healthz", healthz1)
+	srv := http.Server{
+		Addr:    ":80",
+		Handler: mux,
+	}
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Print("Server Started")
+	/*1、sigterm 捕获到了，阻塞->通
+	  2、进行超时控制
+	 */
+	<-done
+	log.Print("Server Stopped")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		// extra handling here
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+	log.Print("Server Exited Properly")
+}
+
+func healthz1(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "ok\n")
+}
+
+func rootHandler1(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("entering root handler")
+	user := r.URL.Query().Get("user")
+	if user != "" {
+		io.WriteString(w, fmt.Sprintf("hello [%s]\n", user))
+	} else {
+		io.WriteString(w, "hello [stranger]\n")
+	}
+	io.WriteString(w, "===================Details of the http request header:============\n")
+	for k, v := range r.Header {
+		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
+	}
+}
